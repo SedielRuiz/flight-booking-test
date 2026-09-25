1188eb7bfcc880d811f8f15f7f5120529581f7b2
Sediel Ruiz <sediel_ruiz@wepay4u.com>
2026-09-25 09:43:43 -0500

    feat: add hsts to backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,26 @@
-import { NestFactory, Reflector } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
-import { AppModule } from './app.module.js';
 import { GlobalExceptionFilter } from '@common/filters/global-exception.filter.js';
-import { ResponseInterceptor } from '@common/interceptors/response.interceptor.js';
 import { BasicAuthGuard } from '@common/guards/basic-auth.guard.js';
+import { ResponseInterceptor } from '@common/interceptors/response.interceptor.js';
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { AppModule } from './app.module.js';
+
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.enableCors();
+
+  app.use(
+    helmet({
+      hsts: {
+        maxAge: 31536000,
+        includeSubDomains: true,
+        preload: true,
+      },
+    }),
+  );
 
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalInterceptors(new ResponseInterceptor());
